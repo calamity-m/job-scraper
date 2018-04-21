13052fbb8619b2c599f57e3a4ff6dee020816594
monteno-m <markmonteno@gmail.com>
2018-04-21 19:25:32 +1000

    added scrape_site method
    added the site scraping method to its own function

--- lambda/aws-simple-scraper/handler.py
+++ lambda/aws-simple-scraper/handler.py
@@ -3,6 +3,47 @@ import requests
 import bs4
 from bs4 import BeautifulSoup
 
+def scrape_site(url):
+    html = requests.get(url)
+    soup = BeautifulSoup(html.text, 'html.parser')
+    
+    jobs = []
+
+    for div in soup.find_all(name="div", attrs={"class": "result"}):
+        
+        # Title
+        title = div.find(name="a", attrs={"data-tn-element":"jobTitle"})["title"]
+        
+        # Company
+        company = div.find(name="span", attrs={"class":"company"}).text.strip()
+        # Location
+        location = div.find(name="span", attrs={"class":"location"}).text.strip()
+        
+        # Description
+        description = div.find(name="span", attrs={"class":"summary"}).text.strip()
+        
+        # Date
+        date = ""
+        try: 
+            date = div.find(name="span", attrs={"class":"date"}).text.strip()
+        except AttributeError as e:
+            date = "N/A"
+        
+        # Salary
+        salary = ""
+        try:
+            salary = div.find(name="span", attrs={"class":"no-wrap"}).text.strip()
+        except AttributeError as e:
+            salary = "N/A"
+        
+        jobs.append({'title': title,
+               'company': company,
+               'location': location,
+               'description': description,
+               'date': date,
+               'salary': salary,})
+
+    return jobs
 
 
 def hello(event, context):
