3cfaa4c032cdc8bc2a7fd68f707a74a38e4f45f7
monteno-m <markmonteno@gmail.com>
2018-05-01 01:12:10 +1000

    Changed colour of header
    Changed header colour to a different hex value

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ import Link from 'gatsby-link'
 const Header = ({ siteTitle }) => (
   <div
     style={{
-      background: 'rebeccapurple',
+      background: '#17182F',
       marginBottom: '1.45rem',
     }}
   >
