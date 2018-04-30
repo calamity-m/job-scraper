05b5b5a978c07147a606539f0f7d6a6a08adcfc7
monteno-m <markmonteno@gmail.com>
2018-04-30 18:41:29 +1000

    Added back extra python files

---
+++ lambda/aws-simple-scraper/scraper.py
@@ -0,0 +1,49 @@
+import requests
+from bs4 import BeautifulSoup
+
+def resolve_span(div, attr):
+    try:
+        rtn = div.find(name="span", attrs=attr)
+        return rtn
+    except:
+        return ""
+
+def scrape_site(url):
+    # HTML page from requests
+    html = requests.get(url)
+    
+    # Inital soup object from bs4
+    soup = BeautifulSoup(html.text, 'html.parser')
+    
+    # Initalize our jobs array
+    jobs = []
+
+    # Scrape through all divs that we want and get job information
+    for div in soup.find_all(name="div", attrs={"class": "result"}):
+        # Title
+        title = div.find(name="a", attrs={"data-tn-element":"jobTitle"})["title"]
+        # Company
+        company = div.find(name="span", attrs={"class":"company"}).text.strip()
+        # Location
+        location = div.find(name="span", attrs={"class":"location"}).text.strip()
+        # Description
+        description = div.find(name="span", attrs={"class":"summary"}).text.strip()
+        # Date
+        date = resolve_span(div, {"class":"date"})
+        if date is None:
+            date = "Not Specified"
+        # Salary
+        salary = resolve_span(div, {"class":"no-wrap"})
+        if salary is None:
+            salary = "Not Specified"
+        
+        # Add a new compiled job to our dict
+        jobs.append({'title': title,
+               'company': company,
+               'location': location,
+               'description': description,
+               'date': date,
+               'salary': salary,})
+
+    # Return our jobs dict
+    return jobs
